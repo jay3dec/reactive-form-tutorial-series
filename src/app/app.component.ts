2a950fa7ee8b76c3ca2ac45d104ecf0b68a50449
jay3dec <jay3dec@gmail.com>
2020-11-18 23:41:25 +0530

    adding project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,20 +7,32 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- constructor(private formBuilder:FormBuilder){}
+  constructor(private formBuilder: FormBuilder) { }
 
- profileForm = this.formBuilder.group({
-   firstName:['',[Validators.required]],
-   lastName:['',[Validators.required]],
-   address:['',[Validators.required]],
-   dob:['',[Validators.required]],
-   gender:['',[Validators.required]]
- });
+  profileForm = this.formBuilder.group({
+    firstName: ['', [Validators.required, this.amperSandValidator]],
+    lastName: ['', [Validators.required]],
+    address: ['', [Validators.required]],
+    dob: ['', [Validators.required]],
+    gender: ['', [Validators.required]]
+  });
 
- saveForm(){
-   if(this.profileForm.valid){
-     console.log('Profile form data :: ', this.profileForm.value);
-   }
- }
+  amperSandValidator(control: FormControl) { (1)
+    let name = control.value;
+    if (name && name.length > 0 && name.indexOf("&") != -1) {
+      return {
+        amperSandError: {
+          enteredName: name
+        }
+      }
+    }
+    return null;
+  }
+
+  saveForm() {
+    if (this.profileForm.valid) {
+      console.log('Profile form data :: ', this.profileForm.value);
+    }
+  }
 
 }
