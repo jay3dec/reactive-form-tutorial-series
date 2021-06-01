1b648a267f273f5f1db388d72a0fbb1de901980d
jay3dec <jay3dec@gmail.com>
2021-06-02 02:31:37 +0530

    adding project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,34 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
  constructor(private formBuilder:FormBuilder){}
 
- profileForm = this.formBuilder.group({
-   firstName:['',[Validators.required]],
-   lastName:['',[Validators.required]],
-   address:['',[Validators.required]],
-   dob:['',[Validators.required]],
-   gender:['',[Validators.required]]
- });
+ profileForm:FormGroup;
+ ngOnInit(){
+   this.profileForm = this.formBuilder.group({
+    firstName:['',[Validators.required]],
+    lastName:['',[Validators.required]],
+    address:[''],
+    dob:['',[Validators.required]],
+    gender:['',[Validators.required]]
+  });
+
+  this.profileForm.get('gender').valueChanges.subscribe(response => {
+    if(response == "male"){
+      this.profileForm.get('address').clearValidators();
+      this.profileForm.get('address').updateValueAndValidity();
+    } else {
+      this.profileForm.get('address').setValidators(Validators.required);
+      this.profileForm.get('address').updateValueAndValidity();
+    }
+  })
+ }
 
  saveForm(){
    if(this.profileForm.valid){
